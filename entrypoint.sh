c6f3b451ae92915a85e1dfd8f240601b2f565021
btthanhk4 <thanhbeo2k4@gmail.com>
2025-07-17 02:33:23 +0700

    update dockerfile va entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Chờ MySQL sẵn sàng trước khi chạy migrate + start
 chmod +x /app/wait-for-it.sh
+/app/wait-for-it.sh "$MYSQL_HOST:$MYSQL_PORT" -t 300 -- echo "MySQL is ready"
 
-/bin/bash /app/wait-for-it.sh $MYSQL_HOST:$MYSQL_PORT -t 300 -- npm start
+# Chạy migrate nếu cần
+npm run migrate
+
+# Khởi chạy ứng dụng
+npm start
